62d8ded271fe766c33d0794d27d49e4c9014ceb2
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-01-16 01:17:22 +0100

    create strategy for dhtmlx

--- app.py
+++ app.py
@@ -124,83 +124,95 @@ def get_specification(query=None):
         specification[parameter] = value
     return specification
 
-def date_to_string(date, timeshift_minutes):
-    """Convert a date to a string.
 
-    - timeshift_minutes is the timeshift specified by the calendar
-        for dates.
-    """
-    # use ISO format
-    # see https://docs.dhtmlx.com/scheduler/howtostart_nodejs.html#step4implementingcrud
-    # see https://docs.python.org/3/library/datetime.html#datetime.datetime.isoformat
-    # see https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
-    timezone = datetime.timezone(datetime.timedelta(minutes=-timeshift_minutes))
-    if isinstance(date, datetime.date) and not isinstance(date, datetime.datetime):
-        date = datetime.datetime(date.year, date.month, date.day, tzinfo=timezone)
-    elif date.tzinfo is None:
-        date = date.replace(tzinfo=timezone)
-    date = date.astimezone(datetime.timezone.utc)
-    return date.strftime("%Y-%m-%d %H:%M")
+class ConvertToDhtmlx:
+    """Convert events to dhtmlx. This conforms to a stratey pattern."""
+    
+    def __init__(self, timeshift_minutes):
+        """Create a DHTMLX conversion strategy.
+        
+        - timeshift_minutes is the timeshift specified by the calendar
+            for dates.
+        """
+        self.timeshift = timeshift_minutes
 
-def convert_ical_event_to_dhtmlx(calendar_event, timeshift):
-    start = calendar_event["DTSTART"].dt
-    end = calendar_event.get("DTEND", calendar_event["DTSTART"]).dt
-    geo = calendar_event.get("GEO", None)
-    if geo:
-        geo = {"lon": geo.longitude, "lat": geo.latitude}
-    name = calendar_event.get("SUMMARY", "")
-    sequence = str(calendar_event.get("SEQUENCE", 0))
-    uid = calendar_event["UID"]
-    start_date = date_to_string(start, timeshift)
-    return {
-        "start_date": start_date,
-        "end_date": date_to_string(end, timeshift),
-        "start_date_iso": start.isoformat(),
-        "end_date_iso": end.isoformat(),
-        "start_date_iso_0": start.isoformat(),
-        "end_date_iso_0": end.isoformat(),
-        "text":  name,
-        "description": calendar_event.get("DESCRIPTION", ""),
-        "location": calendar_event.get("LOCATION", None),
-        "geo": geo,
-        "uid": uid,
-        "ical": calendar_event.to_ical().decode("UTF-8"),
-        "sequence": sequence,
-        "recurrence": None,
-        "url": calendar_event.get("URL"),
-        "id": (uid, start_date),
-        "type": "event"
-    }
+    def date_to_string(self, date):
+        """Convert a date to a string.
 
-def error_to_dhtmlx(ty, error, tb, url=None):
-    """Create an error which can be used by the dhtmlx scheduler."""
-    now = datetime.datetime.now();
-    now_iso = now.isoformat()
-    now_s = date_to_string(now, 0)
-    tb_s = io.StringIO()
-    traceback.print_exception(ty, error, tb, file=tb_s)
-    return {
-        "start_date": now_s,
-        "end_date": now_s,
-        "start_date_iso": now_iso,
-        "end_date_iso": now_iso,
-        "start_date_iso_0": now_iso,
-        "end_date_iso_0": now_iso,
-        "text":  type(error).__name__,
-        "description": str(error),
-        "traceback": tb_s.getvalue(),
-        "location": None,
-        "geo": None,
-        "uid": "error",
-        "ical": "",
-        "sequence": 0,
-        "recurrence": None,
-        "url": url,
-        "id": id(error),
-        "type": "error"
-    }
 
-def retrieve_calendar(url, specification):
+        """
+        # use ISO format
+        # see https://docs.dhtmlx.com/scheduler/howtostart_nodejs.html#step4implementingcrud
+        # see https://docs.python.org/3/library/datetime.html#datetime.datetime.isoformat
+        # see https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+        timezone = datetime.timezone(datetime.timedelta(minutes=-self.timeshift))
+        if isinstance(date, datetime.date) and not isinstance(date, datetime.datetime):
+            date = datetime.datetime(date.year, date.month, date.day, tzinfo=timezone)
+        elif date.tzinfo is None:
+            date = date.replace(tzinfo=timezone)
+        date = date.astimezone(datetime.timezone.utc)
+        return date.strftime("%Y-%m-%d %H:%M")
+
+
+    def convert_ical_event(self, calendar_event):
+        start = calendar_event["DTSTART"].dt
+        end = calendar_event.get("DTEND", calendar_event["DTSTART"]).dt
+        geo = calendar_event.get("GEO", None)
+        if geo:
+            geo = {"lon": geo.longitude, "lat": geo.latitude}
+        name = calendar_event.get("SUMMARY", "")
+        sequence = str(calendar_event.get("SEQUENCE", 0))
+        uid = calendar_event["UID"]
+        start_date = self.date_to_string(start)
+        return {
+            "start_date": start_date,
+            "end_date": self.date_to_string(end),
+            "start_date_iso": start.isoformat(),
+            "end_date_iso": end.isoformat(),
+            "start_date_iso_0": start.isoformat(),
+            "end_date_iso_0": end.isoformat(),
+            "text":  name,
+            "description": calendar_event.get("DESCRIPTION", ""),
+            "location": calendar_event.get("LOCATION", None),
+            "geo": geo,
+            "uid": uid,
+            "ical": calendar_event.to_ical().decode("UTF-8"),
+            "sequence": sequence,
+            "recurrence": None,
+            "url": calendar_event.get("URL"),
+            "id": (uid, start_date),
+            "type": "event"
+        }
+
+    def error(self, ty, error, tb, url=None):
+        """Create an error which can be used by the dhtmlx scheduler."""
+        now = datetime.datetime.now();
+        now_iso = now.isoformat()
+        now_s = self.date_to_string(now)
+        tb_s = io.StringIO()
+        traceback.print_exception(ty, error, tb, file=tb_s)
+        return {
+            "start_date": now_s,
+            "end_date": now_s,
+            "start_date_iso": now_iso,
+            "end_date_iso": now_iso,
+            "start_date_iso_0": now_iso,
+            "end_date_iso_0": now_iso,
+            "text":  type(error).__name__,
+            "description": str(error),
+            "traceback": tb_s.getvalue(),
+            "location": None,
+            "geo": None,
+            "uid": "error",
+            "ical": "",
+            "sequence": 0,
+            "recurrence": None,
+            "url": url,
+            "id": id(error),
+            "type": "error"
+        }
+
+def retrieve_calendar(url, specification, conversion_strategy):
     """Get the calendar entry from a url.
 
     Also unfold the events to past and future.
@@ -218,17 +230,16 @@ def retrieve_calendar(url, specification):
             ical_events.extend(recurring_ical_events.of(calendar).between(one_year_before, one_year_ahead))
         # collect events and their recurrences  
         events = []
-        timeshift = int(specification["timeshift"])
         for calendar_event in ical_events:
-            event = convert_ical_event_to_dhtmlx(calendar_event, timeshift)
+            event = conversion_strategy.convert_ical_event(calendar_event)
             events.append(event)
         return events
     except:
         ty, err, tb = sys.exc_info()
-        error = error_to_dhtmlx(ty, err, tb, url=url)
+        error = conversion_strategy.error(ty, err, tb, url=url)
         return [error]
 
-def get_events(specification):
+def get_events(specification, conversion_strategy):
     """Return events."""
     urls = specification["url"]
     if isinstance(urls, str):
@@ -236,7 +247,7 @@ def get_events(specification):
     assert len(urls) <= MAXIMUM_THREADS, "You can only merge {} urls.".format(MAXIMUM_THREADS)
     all_events = []
     with ThreadPoolExecutor(max_workers=MAXIMUM_THREADS) as e:
-        events_list = e.map(lambda url: retrieve_calendar(url, specification), urls)
+        events_list = e.map(lambda url: retrieve_calendar(url, specification, conversion_strategy), urls)
         for events in events_list:
             all_events.extend(events)
     return all_events
@@ -257,7 +268,7 @@ def get_calendar(type):
     if type == "spec":
         return jsonify(specification)
     if type == "events.json":
-        entries = get_events(specification)
+        entries = get_events(specification, ConvertToDhtmlx(int(specification["timeshift"])))
         return jsonify(entries)
     if type == "html":
         template_name = specification["template"]
