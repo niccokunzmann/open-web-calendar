7232cacd5a407983bdc7a93900cf94e208f78180
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-10-01 15:41:10 +0100

    add Croatian language
    Credits to Tomislav Gomerčić for the translation.

---
+++ static/js/dhtmlx/locale/locale_hr.js
@@ -0,0 +1,15 @@
+/*
+@license
+dhtmlxScheduler v.5.1.6 Professional
+
+This software can be used only as part of dhtmlx.com site.
+You are not allowed to use it on any other site
+
+(c) Dinamenta, UAB.
+
+
+*/
+Scheduler.plugin(function(e){e.locale={date:{month_full:["Siječanj","Veljača","Ožujak","Travanj","Svibanj","Lipanj","Srpanj","Kolovoz","Rujan","Listopad","Studeni","Prosinac"],month_short:["Sij","Velj","Ožu","Tra","Svi","Lip","Srp","Kol","Ruj","Lis","Stu","Pro"],day_full:["Nedjelja","Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak","Subota"],day_short:["Ned","Pon","Uto","Sri","Čet","Pet","Sub"]},labels:{dhx_cal_today_button:"Danas",day_tab:"Dan",week_tab:"Tjedan",month_tab:"Mjesec",new_event:"Novi događaj",icon_save:"Spremi",
+icon_cancel:"Odustani",icon_details:"Detalji",icon_edit:"Uredi",icon_delete:"Izbriši",confirm_closing:"",confirm_deleting:"Događaj će biti izbrisan trajno, da li si siguran?",section_description:"Opis",section_time:"Vremenski period",full_day:"Cijeli dan",confirm_recurring:"Želiš li urediti sve ponavljajuće događaje?",section_recurring:"Ponovi događaj",button_recurring:"Onemogući",button_recurring_open:"Omogući",button_edit_series:"Uredi seriju",button_edit_occurrence:"Uredi događaj",agenda_tab:"Dnevni red",
+date:"Datum",description:"Opis",year_tab:"Godina",week_agenda_tab:"Dnevni red",grid_tab:"Tablica",drag_to_create:"Prenesi za stvoriti novi",drag_to_move:"Prenesi za pomaknuti",message_ok:"U redu",message_cancel:"Odustani",next:"Sljedeći",prev:"Prethodni",year:"Godina",month:"Mjesec",day:"Dan",hour:"Sati",minute:"Minute"}}});
+//# sourceMappingURL=../sources/locale/locale_hr.js.map
