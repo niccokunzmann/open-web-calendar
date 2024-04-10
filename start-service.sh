19d44b5274adeaddde903f18e40b915fd5abe151
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-04-10 16:11:07 +0100

    Deployed 25f4d7c with MkDocs version: 1.5.3

--- start-service.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gunicorn -w "$WORKERS" -b "0.0.0.0:$PORT" app:app
--- templates/locale.js
+++
@@ -1,5 +0,0 @@
-/* Generated locale from the calendar sources. */
-
-Scheduler.plugin(function(e){
-    e.locale = {{ locale }};
-});
