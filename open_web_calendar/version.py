948702d8c1b3bde56f8f1899a5881323ddc248b4
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-07-17 11:08:06 +0100

    reuse

--- open_web_calendar/version.py
+++ open_web_calendar/version.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Nicco Kunzmann and Open Web Calendar Contributors <https://open-web-calendar.quelltext.eu/>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
 try:
     from ._version import __version__, __version_tuple__, version, version_tuple
 except ModuleNotFoundError:
