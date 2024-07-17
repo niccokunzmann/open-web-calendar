409307bd3be93c7d9faab481328e322598a1a70b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-07-17 14:38:54 +0100

    fix renaming refactoring

--- open_web_calendar/version.py
+++ open_web_calendar/version.py
@@ -5,7 +5,7 @@
 try:
     from ._version import __version__, __version_tuple__, version, version_tuple
 except ModuleNotFoundError:
-    __version__ = _version = "0.0dev0"
+    __version__ = version = "0.0dev0"
     __version_tuple__ = version_tuple = (0, 0, "dev0")
 
 __all__ = [
