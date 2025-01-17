8ad3cdea872a2c9b92b6ba487c98b9b21d263424
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-01-17 23:30:12 +0100

    reuse

--- open_web_calendar/test/test_issue_23_choose_map.py
+++ open_web_calendar/test/test_issue_23_choose_map.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Nicco Kunzmann and Open Web Calendar Contributors <https://open-web-calendar.quelltext.eu/>
+#
+# SPDX-License-Identifier: GPL-2.0-only
 """Test choosing the map for the location and geo links."""
 
 from flask.testing import FlaskClient
