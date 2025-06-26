96a6f94bc1f0abc44d404337ecf3242a1e719f28
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-06-26 14:32:50 +0100

    rename file

---
+++ open_web_calendar/translations/fix_po_files.py
@@ -0,0 +1,24 @@
+from pathlib import Path  # noqa: INP001
+from subprocess import check_call
+
+HERE = Path(__file__).parent
+
+
+for po_file in HERE.glob("**/*.po"):
+    # add encoding
+    content = po_file.read_text()
+    print(po_file)  # noqa: T201
+    if not any(
+        line.startswith(('"Content-Type:', 'msgstr "Content-Type:'))
+        for line in content.splitlines()
+    ):
+        content = content.replace(
+            'msgstr ""', 'msgstr ""\n"Content-Type: text/plain; charset=UTF-8\\n"', 1
+        )
+        print("\t added header")  # noqa: T201
+    content = content.replace(
+        '"Content-Type": "text/plain; charset=UTF-8\\n"\n', ""
+    )  # fix old edit
+    po_file.write_text(content)
+    # make content uniq
+    check_call(["msguniq", "-o", po_file, po_file])  # noqa: S603, S607
